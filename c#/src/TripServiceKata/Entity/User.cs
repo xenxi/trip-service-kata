d7df5a3661df15137cd2bc7f01fb5fb61e432c87
Pedro Pardal <ppardalj@gmail.com>
2021-01-20 19:50:51 +0100

    refact: remove dead code add trips in User class

--- c#/src/TripServiceKata/Entity/User.cs
+++ c#/src/TripServiceKata/Entity/User.cs
@@ -4,27 +4,16 @@ namespace TripServiceKata.Entity
 {
     public class User
     {
-        private List<Trip> trips = new List<Trip>();
         private List<User> friends = new List<User>();
 
         public List<User> GetFriends()
         {
             return friends;
-        } 
+        }
 
         public void AddFriend(User user)
         {
             friends.Add(user);
         }
-
-        public void AddTrip(Trip trip)
-        {
-            trips.Add(trip);
-        }
-
-        public List<Trip> Trips()
-        {
-            return trips;
-        } 
     }
 }
