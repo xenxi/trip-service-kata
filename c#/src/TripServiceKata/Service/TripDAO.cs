3b73ee4a00e18e1603a2e3e8ec233a4132edd3ee
andiaz <andiaz@aidacanarias.com>
2021-10-01 07:41:03 +0200

    make method no static

--- c#/src/TripServiceKata/Service/TripDAO.cs
+++ c#/src/TripServiceKata/Service/TripDAO.cs
@@ -6,7 +6,7 @@ namespace TripServiceKata.Service
 {
     public class TripDAO
     {
-        public static List<Trip> FindTripsByUser(User user)
+        public List<Trip> FindTripsByUser(User user)
         {
             throw new DependendClassCallDuringUnitTestException(
                 "TripDAO should not be invoked on an unit test.");
