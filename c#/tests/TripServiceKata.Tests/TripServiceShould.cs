f47447ffe192670fe900c85732e81acc48c2d5f3
andiaz <andiaz@aidacanarias.com>
2021-10-01 07:55:57 +0200

    test green found_trips

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -11,10 +11,12 @@ namespace TripServiceKata.Tests {
     public class TripServiceShould {
         private readonly IUserSession _userSession;
         private readonly TripService _service;
+        private readonly ITripDAO _tripDao;
 
         public TripServiceShould() {
             _userSession = Substitute.For<IUserSession>();
-            _service = new TripService(_userSession, new TripDAO());
+            _tripDao = Substitute.For<ITripDAO>();
+            _service = new TripService(_userSession, _tripDao);
         }
 
         [Fact]
@@ -36,16 +38,18 @@ namespace TripServiceKata.Tests {
         }
 
         [Fact]
-        public void fail()
+        public void found_trips()
         {
             var aGivenLoggedUser = new User();
             var aGivenUser = new User();
             aGivenUser.AddFriend(aGivenLoggedUser);
             _userSession.GetLoggedUser().Returns(aGivenLoggedUser);
+            List<Trip> aExpectedListOfTrip = new List<Trip>();
+            _tripDao.FindTripsByUser(aGivenUser).Returns(aExpectedListOfTrip);
 
             var trips =  _service.GetTripsByUser(aGivenUser);
 
-            trips.Should().BeSameAs(new List<Trip>());
+            trips.Should().BeSameAs(aExpectedListOfTrip);
         }
     }
 }
