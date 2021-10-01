eaf3be102309b2946a639151ad3855f5ba6c7fab
andiaz <andiaz@aidacanarias.com>
2021-10-01 11:26:33 +0200

    add test: add_a_friend

---
+++ c#/tests/TripServiceKata.Tests/UserShould.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TripServiceKata.Tests
+{
+    public  class UserShould
+    {
+        [Fact]
+        public void add_a_friend() {
+            var aGivenUser = UserMother.Create();
+            var aGivenFriend = UserMother.Create();
+
+            aGivenUser.AddFriend(aGivenFriend);
+
+            aGivenUser.GetFriends().Should().Contain(aGivenFriend);
+        }
+    }
+}
