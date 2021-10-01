84a098cc9c278ed47c8bdb6ce62b58bc721f4888
andiaz <andiaz@aidacanarias.com>
2021-10-01 07:17:51 +0200

    add first characterization test

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,16 @@
-﻿namespace TripServiceKata.Tests
+﻿using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void fail() {
+            var service = new TripService();
+
+            service.GetTripsByUser(null);
+
+            Assert.True(false);
+        }
     }
 }
